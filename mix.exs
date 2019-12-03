1e6e58146c7b9b8237b6abbfc7bc2691919fa226
Dmitry <dmitry.shpagin@balance-pl.ru>
2019-12-03 09:41:03 +0300

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxKeycloak.MixProject do
   def project do
     [
       app: :phx_keycloak,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
