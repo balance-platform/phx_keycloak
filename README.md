43fedd40b7f9382834dc45a7450fe72d87cfd778
Dmitry Shpagin <dmitry.shpagin@balance-pl.ru>
2021-02-23 15:24:24 +0300

    Hex publish

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # PhxKeycloak
 
-Библиотека, отвечающая за проверку прав доступа к приложению с помощью системы Keycloak
+[![github.com](https://github.com/balance-platform/phx_keycloak/workflows/build/badge.svg?branch=master)](https://github.com/balance-platform/phx_keycloak/actions)
+[![hex.pm](https://img.shields.io/badge/docs-hexpm-blue.svg)](https://hexdocs.pm/phx_keycloak)
+[![hex.pm](https://img.shields.io/hexpm/v/phx_keycloak.svg)](https://hex.pm/packages/phx_keycloak)
+[![hex.pm](https://img.shields.io/hexpm/dt/phx_keycloak.svg)](https://hex.pm/packages/phx_keycloak)
+[![hex.pm](https://img.shields.io/hexpm/l/phx_keycloak.svg)](https://hex.pm/packages/phx_keycloak)
+[![github.com](https://img.shields.io/github/last-commit/balance-platform/phx_keycloak.svg)](https://github.com/balance-platform/phx_keycloak/commits/master)
+
+Library helps to setup SSO integration via Keycloak
+
+This is not very simple, full instruction can be found at 
 
 Краткую инструкцию по установке и запуску можно найти в документации к модулю PhxKeycloak
 
--- mix.exs
+++ mix.exs
@@ -1,22 +1,34 @@
 defmodule PhxKeycloak.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/balance-platform/phx_jeycloak"
+  @version "0.2.2"
+
   def project do
     [
       app: :phx_keycloak,
-      version: "0.2.2",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
       # Docs
       name: "PhxKeycloak",
-      source_url: nil,
-      homepage_url: nil,
-      docs: [
-        main: "PhxKeycloak",
-        extras: ["README.md"]
-      ]
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      description: description(),
+      package: package(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_add_deps: :transitive
+      ],
+      homepage_url: @source_url,
+      docs: docs()
     ]
   end
 
@@ -40,11 +52,30 @@ defmodule PhxKeycloak.MixProject do
     ]
   end
 
+  defp description do
+    """
+    Elixir Keycloak integration, based on plugs and macroses.
+    Mostly compatible with phoenix apps
+    """
+  end
+
   defp package do
     [
-      maintainers: ["Dmitry Shpagin"],
+      # This option is only needed when you don't want to use the OTP application name
+      name: "phx_keycloak",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
       licenses: ["MIT"],
-      files: ~w(assets/css assets/js lib priv mix.exs README.md)
+      links: %{"GitHub" => "https://github.com/balance-platform/phx_keycloak"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md"]
     ]
   end
 end
